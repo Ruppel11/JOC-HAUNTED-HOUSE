03a259e4da462bb3294a0d7fe6144203af5f8da1
Ruppel11 <165767918+Ruppel11@users.noreply.github.com>
2024-05-23 22:42:54 +0200

    Initial commit

---
+++ character_body_2d.gd
@@ -0,0 +1,92 @@
+extends CharacterBody2D
+
+var fantasmas_inattack_range = false
+var fantasmas_attack_cooldown = true
+var health = 100
+var player_alive = true
+
+var num_key = 0
+const max_speed = 400.0
+const accel = 1500.0
+const friction = 600
+@onready var animated_sprite_2d = $AnimatedSprite2D
+
+@onready var MenuPause : Node2D = $MenuPause
+var input = Vector2.ZERO
+
+func _physics_process(delta):
+	# Add the gravity.
+	animacion()
+	fantasmas_attack()
+	
+	if health <= 0:
+		player_alive = false # mort del personatge
+		health = 0
+		print("player has been killed")
+		self.queue_free()
+		get_tree().change_scene_to_file("res://game_over.tscn")
+		
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var directionx = Input.get_axis("ui_left", "ui_right")
+	if directionx:
+		velocity.x = directionx * max_speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, max_speed)
+	var directiony = Input.get_axis("up", "down")
+	if directiony:
+		velocity.y = directiony * max_speed
+	else:
+		velocity.y = move_toward(velocity.y, 0, max_speed)
+	
+	move_and_slide()
+
+func animacion():
+		
+	if velocity.x < 0:
+		animated_sprite_2d.animation = "esquerra"
+		$AnimatedSprite2D.flip_h = false
+	if velocity.x > 0:
+		animated_sprite_2d.animation = "esquerra"
+		$AnimatedSprite2D.flip_h = true
+	if velocity.y < 0:
+		animated_sprite_2d.animation = "adalt"
+			
+	if velocity.y == 0 and velocity.x == 0:
+		animated_sprite_2d.animation = "quiet"
+	# Llama a la función de animación en cada frame
+	
+func _on_pause_button_pressed():
+	get_tree().paused = true
+	MenuPause.visible = get_tree().paused
+
+func _on_restart_pressed():
+	get_tree().reload_current_scene()
+
+func _on_resum_pressed():
+	get_tree().paused = false
+	MenuPause.visible = get_tree().paused
+
+func player():
+	pass
+
+func _on_player_hitbox_area_entered(area):
+	if area.is_in_group("fantasmas"):
+		fantasmas_inattack_range = true
+
+func _on_player_hitbox_area_exited(area):
+	if area.is_in_group("fantasmas"):
+		fantasmas_inattack_range = false
+		
+func fantasmas_attack():
+	if fantasmas_inattack_range and fantasmas_attack_cooldown == true:
+		health -= 20
+		fantasmas_attack_cooldown = false
+		$attack_cooldown.start()
+		print(health)
+
+func _on_atack_cooldown_timeout():
+	fantasmas_attack_cooldown = true
+
+
+
